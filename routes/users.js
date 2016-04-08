0a53094bd783a99f04d5194b46958cd7f26a5dad
Rawad Alawar <email": null>
2016-04-09 11:39:01 +1200

    adds user registration route

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/register', function(req, res, next) {     //route for users/register since already in users.js
+  res.render('register', {
+    'title': 'Register'
+  });
+});
+
+router.get('/login', function(req, res, next) {     //route for users/register since already in users.js
+  res.render('login', {
+    'title': 'Login'
+  });
+});
+
 module.exports = router;
